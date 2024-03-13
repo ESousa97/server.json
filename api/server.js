0c1b5f67ab660ee751c87025c73fa317e08a2ff9
ESousa97 <xfuuuuuuuuury2018@gmail.com>
2024-03-13 16:06:40 -0300

    att rotas

---
+++ api/server.js
@@ -0,0 +1,8 @@
+// api/server.js
+
+const cardlist = require('./cardlist');
+const categories = require('./categories');
+const procedure = require('./procedure');
+const search = require('./search');
+
+module.exports = { cardlist, categories, procedure, search };
